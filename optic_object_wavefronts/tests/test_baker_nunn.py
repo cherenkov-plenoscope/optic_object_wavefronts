f43e9fad0c93ddfcdc6da68453300cbc61e09624
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-02-19 10:49:21 +0100

    optics according to fors2013telescope

--- optic_object_wavefronts/tests/test_baker_nunn.py
+++ optic_object_wavefronts/tests/test_baker_nunn.py
@@ -5,7 +5,34 @@ def test_baker_nunn():
     # baker nunn
     n_polygon = 71
     n_hex_grid = 17
+
     corrector_radius = 0.508 / 2.0
+    mirror_radius = 0.7600 / 2.0
+    mirror_curvature_radius = 1.016167
+    mirror_thickness = 0.05
+
+    mirr_to_corr3 = 0.9448
+    corr3_width = 0.02654
+    corr1_width = corr3_width
+    corr2_width = 0.01425
+    corr3_to_corr2 = 0.0492
+    corr2_to_corr1 = corr3_to_corr2
+
+    z_mirror = 0.0
+    z_corr3 = mirr_to_corr3 + corr3_width
+    z_corr2 = z_corr3 + corr3_to_corr2 + corr2_width
+    z_corr1 = z_corr2 + corr2_to_corr1 + corr1_width
+
+    mirror = oow.primitives.spherical_lens.make_mesh(
+        outer_radius=mirror_radius,
+        curvature_radius_top=-mirror_curvature_radius,
+        curvature_radius_bot=-mirror_curvature_radius - mirror_thickness,
+        offset=mirror_thickness,
+        n_polygon=n_polygon,
+        n_hex_grid=n_hex_grid,
+        rot=0.0,
+        ref="mirror",
+    )
 
     cor1 = oow.primitives.spherical_lens.make_mesh(
         outer_radius=corrector_radius,
@@ -40,20 +67,27 @@ def test_baker_nunn():
         ref="corrector3",
     )
 
-    corrector = oow.Mesh.init()
-    corrector = oow.Mesh.merge(
-        corrector,
-        oow.Mesh.translate(cor1, np.array([0.0, 0.0, 0.0]))
+    baker_nunn = oow.Mesh.init()
+
+    baker_nunn = oow.Mesh.merge(
+        baker_nunn,
+        oow.Mesh.translate(mirror, np.array([0.0, 0.0, z_mirror]))
     )
 
-    corrector = oow.Mesh.merge(
-        corrector,
-        oow.Mesh.translate(cor2, np.array([0.0, 0.0, - 1 * 0.0492]))
+    baker_nunn = oow.Mesh.merge(
+        baker_nunn,
+        oow.Mesh.translate(cor3, np.array([0.0, 0.0, z_corr3]))
     )
 
-    corrector = oow.Mesh.merge(
-        corrector,
-        oow.Mesh.translate(cor3, np.array([0.0, 0.0, - 2 * 0.0492 - 0.01425]))
+
+    baker_nunn = oow.Mesh.merge(
+        baker_nunn,
+        oow.Mesh.translate(cor2, np.array([0.0, 0.0, z_corr2]))
     )
 
-    oow.write_mesh("baker_nunn.obj", corrector)
+    baker_nunn = oow.Mesh.merge(
+        baker_nunn,
+        oow.Mesh.translate(cor1, np.array([0.0, 0.0, z_corr1]))
+    )
+
+    oow.write_mesh("baker_nunn.obj", baker_nunn)
