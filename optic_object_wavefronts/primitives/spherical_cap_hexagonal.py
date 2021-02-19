2eeb9335a699b63b803d6f5ce901d016c0ae9eb6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-02-19 21:51:33 +0100

    hex mirror body

--- optic_object_wavefronts/primitives/spherical_cap_hexagonal.py
+++ optic_object_wavefronts/primitives/spherical_cap_hexagonal.py
@@ -1,7 +1,10 @@
 from .. import Mesh
 from .. import delaunay
 from .. import spherical
+from . import disc
+from .. import regular_polygon
 from .. import hexagonal_grid
+import copy
 import numpy as np
 
 
@@ -47,4 +50,110 @@ def make_mesh(
         }
 
     mesh["materials"][ref] = [ref]
+    return mesh
+
+
+def rotate_vertices_xy(vertices, phi):
+    cosp = np.cos(phi)
+    sinp = np.sin(phi)
+    vertices_out = copy.deepcopy(vertices)
+    for vkey in vertices_out:
+        x = vertices_out[vkey][0]
+        y = vertices_out[vkey][1]
+        nx = cosp * x - sinp * y
+        ny = sinp * x + cosp * y
+        vertices_out[vkey][0] = nx
+        vertices_out[vkey][1] = ny
+    return vertices_out
+
+
+def make_front_spherical_back_plane_mesh(
+    outer_radius,
+    curvature_radius,
+    width,
+    n=10,
+    ref="SphericalPlaneHexagonalBody"
+):
+    front_mesh = make_mesh(
+        outer_radius=outer_radius,
+        curvature_radius=curvature_radius,
+        n=n,
+        ref=ref + "/front"
+    )
+
+    back_mesh = disc.make_mesh(
+        outer_radius=outer_radius,
+        n=6,
+        ref=ref + "/back",
+        rot=0.0,
+    )
+
+    center_of_curvature = np.array([0.0, 0.0, curvature_radius])
+
+    back_mesh = Mesh.translate(back_mesh, [0.0, 0.0, -width])
+    for vnkey in back_mesh["vertex_normals"]:
+        back_mesh["vertex_normals"][vnkey] = np.array([0.0, 0.0, -1.0])
+
+    mesh = Mesh.merge(front_mesh, back_mesh)
+
+    inner_radius_hexagon = outer_radius * regular_polygon.inner_radius(n=6)
+
+    rot_perp = np.pi / 2.0
+
+    for irotz, phi in enumerate(np.linspace(0, 2*np.pi, 6, endpoint=False)):
+        i_vertices = rotate_vertices_xy(vertices=mesh["vertices"], phi=phi)
+
+        i_combi_vertices = {}
+        for fkey in i_vertices:
+            if i_vertices[fkey][1] > 0.99 * inner_radius_hexagon:
+                i_combi_vertices[fkey] = np.array([
+                    i_vertices[fkey][0],
+                    i_vertices[fkey][2],
+                    0.0,
+                ])
+
+        i_faces = delaunay.make_faces_xy(
+            vertices=i_combi_vertices,
+            ref=ref + "/side_{:6}".format(irotz)
+        )
+
+        i_normal = np.array([
+            np.cos(-phi + rot_perp),
+            np.sin(-phi + rot_perp),
+            0.0
+        ])
+        i_vnkey = (ref + "/side", irotz)
+
+        mesh["vertex_normals"][i_vnkey] = i_normal
+
+        for fkey in i_faces:
+
+            va = mesh["vertices"][i_faces[fkey]["vertices"][0]]
+            vb = mesh["vertices"][i_faces[fkey]["vertices"][1]]
+            vc = mesh["vertices"][i_faces[fkey]["vertices"][2]]
+
+            mid_ab = 0.5 * (va + vb)
+            mid_bc = 0.5 * (vb + vc)
+            mid_ca = 0.5 * (vc + va)
+
+            r_mid_ab = np.linalg.norm(mid_ab - center_of_curvature)
+            r_mid_bc = np.linalg.norm(mid_bc - center_of_curvature)
+            r_mid_ca = np.linalg.norm(mid_ca - center_of_curvature)
+
+            mid_ab_inside = r_mid_ab <= center_of_curvature
+            mid_bc_inside = r_mid_bc <= center_of_curvature
+            mid_ca_inside = r_mid_ca <= center_of_curvature
+
+            if np.sum([mid_ab_inside, mid_bc_inside, mid_ca_inside]) < 2:
+
+                mesh["faces"][fkey] = {
+                    "vertices": i_faces[fkey]["vertices"],
+                    "vertex_normals": [i_vnkey, i_vnkey, i_vnkey],
+                }
+
+    mesh["materials"] = {}
+    mesh["materials"][ref+"_front"] = [ref + "/front"]
+    mesh["materials"][ref+"_back"] = [ref + "/back"]
+    mesh["materials"][ref+"_side"] = [ref + "/side"]
+
     return mesh
