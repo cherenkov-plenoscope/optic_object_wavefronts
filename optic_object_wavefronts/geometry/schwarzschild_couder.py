3bea1ab81c4d691be6685e88378ac3af319cd110
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-11-08 17:41:08 +0100

    these are z-values

--- optic_object_wavefronts/geometry/schwarzschild_couder.py
+++ optic_object_wavefronts/geometry/schwarzschild_couder.py
@@ -23,11 +23,11 @@ def surface_normal(x, y, k, c, a1, a2, a3, a4, a5, a6, a7, a8, delta=1e-6):
     xm = x - delta
     yp = y + delta
     ym = y - delta
-    xp = surface_height(xp, y, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
-    xm = surface_height(xm, y, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
-    yp = surface_height(x, yp, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
-    ym = surface_height(x, ym, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
-    dzdx = (xp - xm) / (2 * delta)
-    dzdy = (yp - ym) / (2 * delta)
+    z_xp = surface_height(xp, y, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
+    z_xm = surface_height(xm, y, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
+    z_yp = surface_height(x, yp, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
+    z_ym = surface_height(x, ym, k, c, a1, a2, a3, a4, a5, a6, a7, a8)
+    dzdx = (z_xp - z_xm) / (2 * delta)
+    dzdy = (z_yp - z_ym) / (2 * delta)
     normal = [-dzdx, -dzdy, 1.0]
     return normal/np.linalg.norm(normal)
