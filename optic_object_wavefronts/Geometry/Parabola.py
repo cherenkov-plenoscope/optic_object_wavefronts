abfad4b7b271438e3939feeb5d0b6153f4ace370
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-07-13 22:08:33 +0200

    make perfect parabolas

--- optic_object_wavefronts/Geometry/Parabola.py
+++ optic_object_wavefronts/Geometry/Parabola.py
@@ -1,13 +1,18 @@
-from . import Ellipse
+import numpy as np
 
 
 def surface_height(x, y, focal_length):
-    return Ellipse.surface_height(
-        x=x, y=y, focal_length_x=focal_length, focal_length_y=focal_length
-    )
+    r = np.hypot(x, y)
+    return r ** 2 / (4.0 * focal_length)
 
 
 def surface_normal(x, y, focal_length):
-    return Ellipse.surface_normal(
-        x=x, y=y, focal_length_x=focal_length, focal_length_y=focal_length
-    )
+    """
+    surface normal is given as  ( -dz/dx , -dz/dy , 1 )
+
+    z(x,y) = (x**2 + y**2)/(4f)
+    """
+    dzdx = (2.0*x)/(4.0*focal_length)
+    dzdy = (2.0*y)/(4.0*focal_length)
+    normal = np.array([-dzdx, -dzdy, 1.0])
+    return normal / np.linalg.norm(normal)
