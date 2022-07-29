3bb1981b6e19a9ab4bb05d1285a151e96a3eec26
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-07-29 16:22:09 +0200

    allow to set custom colors on the fly

--- optic_object_wavefronts/materials/__init__.py
+++ optic_object_wavefronts/materials/__init__.py
@@ -11,9 +11,21 @@ MEDIA_DIR = os.path.join(MATERIALS_DIR, "media")
 
 
 def surface(key):
-    path = os.path.join(SURFACES_DIR, key + ".json")
+    RGB = "/" in key
+
+    basic_key = os.path.dirname(key) if RGB else key
+
+    path = os.path.join(SURFACES_DIR, basic_key + ".json")
     with open(path, "rt") as f:
         c = json.loads(f.read())
+
+    if RGB:
+        rgb_key = os.path.basename(key)
+        rgb = str.split(rgb_key, "_")
+        assert rgb[0] == "rgb"
+        assert len(rgb) == 4
+        rgb = rgb[1:]
+        c["color"] = [int(i) for i in rgb]
     return c
 
 
