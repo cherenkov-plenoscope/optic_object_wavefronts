4bf8d39f4403656aa21826efc0a63121245e23e1
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-03-16 19:08:42 +0100

    new feature, schmidt corrector! now with code!

---
+++ optic_object_wavefronts/primitives/schmidt_corrector_plate.py
@@ -0,0 +1,82 @@
+import numpy as np
+import posixpath
+import collections
+from .. import mesh
+from . import template_cylinder
+from . import plane
+from . import schmidt_corrector_surface
+
+
+def init(
+    outer_polygon,
+    inner_polygon,
+    schmidt_corrector,
+    offset,
+    fn_hex_grid,
+    ref="schmidt_corrector_plate",
+    inner_radius=None,
+):
+    top = schmidt_corrector_surface.init(
+        outer_polygon=outer_polygon,
+        schmidt_corrector_curvature_config=schmidt_corrector_curvature_config,
+        inner_polygon=inner_polygon,
+        fn_hex_grid=fn_hex_grid,
+        ref=posixpath.join(ref, "to_mirror"),
+    )
+    bot = plane.init(
+        outer_polygon=outer_polygon,
+        inner_polygon=inner_polygon,
+        ref=posixpath.join(ref, "to_object"),
+        fn_hex_grid=fn_hex_grid,
+    )
+
+    lens = mesh.init()
+
+    for vkey in top["vertices"]:
+        tmp_v = np.array(top["vertices"][vkey])
+        # tmp_v[2] = tmp_v[2] + 0.5 * float(offset)
+        lens["vertices"][vkey] = tmp_v
+
+    top_mtl_key = posixpath.join(ref, "top")
+    lens["materials"][top_mtl_key] = collections.OrderedDict()
+    for fkey in top["materials"][top_mtl_key]:
+        lens["materials"][top_mtl_key][fkey] = top["materials"][top_mtl_key][
+            fkey
+        ]
+
+    for vnkey in top["vertex_normals"]:
+        lens["vertex_normals"][vnkey] = +1.0 * top["vertex_normals"][vnkey]
+
+    for vkey in bot["vertices"]:
+        tmp_v = np.array(bot["vertices"][vkey])
+        tmp_v[2] = tmp_v[2] - float(offset)
+        lens["vertices"][vkey] = tmp_v
+
+    bot_mtl_key = posixpath.join(ref, "bot")
+    lens["materials"][bot_mtl_key] = collections.OrderedDict()
+    for fkey in bot["materials"][bot_mtl_key]:
+        lens["materials"][bot_mtl_key][fkey] = bot["materials"][bot_mtl_key][
+            fkey
+        ]
+
+    for vnkey in bot["vertex_normals"]:
+        lens["vertex_normals"][vnkey] = -1.0 * bot["vertex_normals"][vnkey]
+
+    lens = template_cylinder.weave_cylinder_faces(
+        mesh=lens,
+        vkey_lower=posixpath.join(ref, "bot", "outer_bound"),
+        vkey_upper=posixpath.join(ref, "top", "outer_bound"),
+        ref=posixpath.join(ref, "outer"),
+        norm_sign=+1.0,
+    )
+
+    if inner_radius is not None:
+        lens = template_cylinder.weave_cylinder_faces(
+            mesh=lens,
+            vkey_lower=posixpath.join(ref, "bot", "inner_bound"),
+            vkey_upper=posixpath.join(ref, "top", "inner_bound"),
+            ref=posixpath.join(ref, "inner"),
+            norm_sign=-1.0,
+        )
+
+    return lens
